42786de3fd7630118d35df41cdd77c6a3d99fe54
Samir Bagamaev <05ru4315@gmail.com>
2022-12-23 17:13:33 +0300

    Программа,которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве

---
+++ ConsoleApp_003/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и 
+// находит расстояние между ними в 2D пространстве.
+
+Console.WriteLine("Введите координаты точки X ");
+int X = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координаты точки Y ");
+int Y = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координаты первой точки X1 ");
+int X1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координаты второй точки Y1 ");
+int Y1 = Convert.ToInt32(Console.ReadLine());
+double result = Math.Sqrt(Math.Pow(X1 - X, 2) + Math.Pow(Y1 - Y, 2));
+
+Console.WriteLine($"Расстояние между точками =  {result}");
